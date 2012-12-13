211b454396579dba92575c4d0372dd57719b30e9
Liu Jiyang <2411mail@gmail.com>
2012-12-13 10:18:22 +0800

    setup new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfflineLuckyyang::Application.config.secret_token = '0401b209cdf7d6896285ff06516004c8be497d330f9abcd4327007644cfd496e263959306925bf72874bfe6b5e144b44057d7022db2b3d38410185023aa3bfeb'
